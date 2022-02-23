cd3b9881896c69722027ec0daf371c754f15a8e2
StartsMercury <startsmercury@gmail.com>
2022-02-23 18:25:58 +0800

    Replaced Examples
     - replaced example author
     - replaced example description
     - replaced example id
     - replaced example package

--- src/main/java/com/github/startsmercury/brightnessautotune/ExampleMod.java
+++ src/main/java/com/github/startsmercury/brightnessautotune/ExampleMod.java
@@ -1,4 +1,4 @@
-package net.fabricmc.example;
+package com.github.startsmercury.brightnessautotune;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
--- src/main/java/com/github/startsmercury/brightnessautotune/mixin/ExampleMixin.java
+++ src/main/java/com/github/startsmercury/brightnessautotune/mixin/ExampleMixin.java
@@ -1,11 +1,12 @@
-package net.fabricmc.example.mixin;
+package com.github.startsmercury.brightnessautotune.mixin;
 
-import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.github.startsmercury.brightnessautotune.ExampleMod;
 
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
