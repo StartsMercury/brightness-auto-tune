2ce092a434a0ad62958f8c86883788e58a514a66
StartsMercury <startsmercury@gmail.com>
2022-02-23 18:52:42 +0800

    Official Mojang Mappings
     - no longer use loom mappings

--- src/main/java/com/github/startsmercury/brightnessautotune/mixin/ExampleMixin.java
+++ src/main/java/com/github/startsmercury/brightnessautotune/mixin/ExampleMixin.java
@@ -1,6 +1,5 @@
 package com.github.startsmercury.brightnessautotune.mixin;
 
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -8,10 +7,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.github.startsmercury.brightnessautotune.ExampleMod;
 
+import net.minecraft.client.gui.screens.TitleScreen;
+
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
+	private void init(final CallbackInfo info) {
 		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
