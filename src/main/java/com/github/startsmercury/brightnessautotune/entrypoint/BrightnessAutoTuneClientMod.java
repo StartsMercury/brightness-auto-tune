f605a8460c6af73748df17746033771baae944f7
StartsMercury <startsmercury@gmail.com>
2022-02-23 20:51:10 +0800

    Brightness Auto Tune I
     - no interpolation
     - not sublte

---
+++ src/main/java/com/github/startsmercury/brightnessautotune/entrypoint/BrightnessAutoTuneClientMod.java
@@ -0,0 +1,28 @@
+package com.github.startsmercury.brightnessautotune.entrypoint;
+
+import static net.fabricmc.api.EnvType.CLIENT;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Options;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+
+@Environment(CLIENT)
+public class BrightnessAutoTuneClientMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			final Options options = client.options;
+			final ClientLevel level = client.level;
+			final LocalPlayer player = client.player;
+
+			if (level != null && player != null) {
+				options.gamma = (15 - level.getRawBrightness(player.blockPosition(), 0)) / 15.0D;
+			} else {
+				options.gamma = 0.5D;
+			}
+		});
+	}
+}
