563779c165e4a1c9a8c71723810b60798ffb6793
StartsMercury <startsmercury@gmail.com>
2022-03-05 13:32:18 +0800

    Minor Code Changes
     - fixed inverted tuning on potion effects
     - checks for conduit effect

--- src/main/java/com/github/startsmercury/brightnessautotune/api/BrightnessSampler.java
+++ src/main/java/com/github/startsmercury/brightnessautotune/api/BrightnessSampler.java
@@ -4,9 +4,11 @@ import static java.lang.Math.abs;
 import static java.lang.Math.max;
 import static net.fabricmc.api.EnvType.CLIENT;
 import static net.minecraft.world.effect.MobEffects.BLINDNESS;
+import static net.minecraft.world.effect.MobEffects.CONDUIT_POWER;
 import static net.minecraft.world.effect.MobEffects.NIGHT_VISION;
 import static net.minecraft.world.level.LightLayer.BLOCK;
 import static net.minecraft.world.level.LightLayer.SKY;
+import static net.minecraft.world.level.material.Fluids.WATER;
 
 import net.fabricmc.api.Environment;
 import net.minecraft.core.BlockPos;
@@ -19,19 +21,25 @@ import net.minecraft.world.phys.Vec3;
 public interface BrightnessSampler {
 	BrightnessSampler LEGACY = (level, player, position) -> {
 		if (player.hasEffect(BLINDNESS) /* || player.hasEffect(MobEffects.DARKNESS) */) {
-			return 1.0D;
-		} else if (player.isOnFire() || player.hasEffect(NIGHT_VISION)) {
 			return 0.0D;
-		} else {
-			final BlockPos blockPos = new BlockPos(position);
-
-			final DimensionType dimensionType = level.dimensionType();
-			final int blockLight = level.getBrightness(BLOCK, blockPos);
-			final int skyLight = level.getBrightness(SKY, blockPos) - level.getSkyDarken();
-			final double sun = 2.0D * abs(0.5D - level.getTimeOfDay(0.0F));
-
-			return max(sun * dimensionType.brightness(skyLight), dimensionType.brightness(blockLight));
 		}
+
+		if (player.isOnFire() || player.hasEffect(NIGHT_VISION)) {
+			return 1.0D;
+		}
+
+		final BlockPos blockPos = new BlockPos(position);
+
+		if (player.hasEffect(CONDUIT_POWER) && level.getFluidState(blockPos).is(WATER)) {
+			return 1.0D;
+		}
+
+		final DimensionType dimensionType = level.dimensionType();
+		final int blockLight = level.getBrightness(BLOCK, blockPos);
+		final int skyLight = level.getBrightness(SKY, blockPos) - level.getSkyDarken();
+		final double sun = 2.0D * abs(0.5D - level.getTimeOfDay(0.0F));
+
+		return max(sun * dimensionType.brightness(skyLight), dimensionType.brightness(blockLight));
 	};
 
 	double sampleBrightness(final Level level, final Player player, final Vec3 position);
--- src/main/java/com/github/startsmercury/brightnessautotune/api/TuneMethod.java
+++ src/main/java/com/github/startsmercury/brightnessautotune/api/TuneMethod.java
@@ -1,10 +1,15 @@
 package com.github.startsmercury.brightnessautotune.api;
 
+import static net.fabricmc.api.EnvType.CLIENT;
+
 import java.util.function.DoubleSupplier;
 
+import net.fabricmc.api.Environment;
+
+@Environment(CLIENT)
 public interface TuneMethod {
-	TuneMethod LEGACY = (currentBrightness, targetBrightness,
-			multiplier) -> (currentBrightness + multiplier.getAsDouble() * (targetBrightness - currentBrightness));
+	TuneMethod LEGACY = (currentBrightness, targetBrightness, multiplier) -> currentBrightness +
+			multiplier.getAsDouble() * (targetBrightness - currentBrightness);
 
 	double tune(double currentBrightness, double targetBrightness, DoubleSupplier multiplier);
 }
